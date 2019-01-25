b6d7811093fd885116c67eecc6f5f09afb5141b1
Thibault <933kss@gmail.com>
2019-01-25 13:34:50 +0900

    add rails version to migrations

--- db/migrate/20170909015940_add_private_by_default_to_projects.rb
+++ db/migrate/20170909015940_add_private_by_default_to_projects.rb
@@ -1,4 +1,4 @@
-class AddPrivateByDefaultToProjects < ActiveRecord::Migration
+class AddPrivateByDefaultToProjects < ActiveRecord::Migration[5.2]
   def change
     add_column :projects, :issues_private_by_default, :boolean, default: false
   end
--- db/migrate/20171027024200_add_notes_private_by_default_to_projects.rb
+++ db/migrate/20171027024200_add_notes_private_by_default_to_projects.rb
@@ -1,4 +1,4 @@
-class AddNotesPrivateByDefaultToProjects < ActiveRecord::Migration
+class AddNotesPrivateByDefaultToProjects < ActiveRecord::Migration[5.2]
   def change
     add_column :projects, :issue_notes_private_by_default, :boolean, default: false
   end
