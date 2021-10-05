c7a4098d6245ede630b56171a644a57cfcd8be35
Ko Nagase <nagase@georepublic.co.jp>
2021-10-05 17:59:07 +0900

    Switched unit test user from 'John Smith' to 'Dave Lopper' and added tests for view_members_displayname permission

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,18 +1,18 @@
 require_relative '../test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  fixtures :email_addresses, :users
+  fixtures :email_addresses, :users, :roles, :projects, :members, :member_roles
 
   setup do
-    @user = User.find(2)
-    @user.update_column :displayname, 'smitty'
+    @user = User.find(3)
+    @user.update_column :displayname, 'davie'
   end
 
   test 'should mask out name for unprivileged user' do
     User.current = User.anonymous
     with_settings(user_format: 'firstname_lastname') do
       user = User.find @user.id
-      assert_equal 'User', user.name
+      assert_equal 'Developer', user.name
     end
   end
 
@@ -21,12 +21,12 @@ class UserTest < ActiveSupport::TestCase
 
     with_settings(user_format: 'firstname_lastname') do
       user = User.find @user.id
-      assert_equal 'John Smith', user.name
+      assert_equal 'Dave Lopper', user.name
     end
 
     with_settings(user_format: 'displayname') do
       user = User.find @user.id
-      assert_equal 'smitty', user.name
+      assert_equal 'davie', user.name
     end
   end
 
@@ -38,7 +38,7 @@ class UserTest < ActiveSupport::TestCase
       plugin_redmine_privacy: { 'displayname_fallback' => 'login' }
     ) do
       user = User.find @user.id
-      assert_equal 'John Smith', user.name
+      assert_equal 'Dave Lopper', user.name
     end
 
     with_settings(
@@ -48,13 +48,49 @@ class UserTest < ActiveSupport::TestCase
       assert_equal :username, RedminePrivacy.displayname_fallback
 
       user = User.find @user.id
-      assert_equal 'smitty', user.name
+      assert_equal 'davie', user.name
 
       user = User.find @user.id
       user.update_column :displayname, ''
-      assert_equal 'jsmith', user.name
+      assert_equal 'dlopper', user.name
     end
-
   end
+
+  test 'should show display name of same project member with permission' do
+    Role.find(1).add_permission! :view_members_displayname
+    User.current = User.find(2)
+    with_settings(
+      user_format: 'displayname',
+      plugin_redmine_privacy: { 'displayname_fallback' => 'lastname' }
+    ) do
+      user = User.find @user.id
+      assert_equal 'davie', user.name
+    end
+  end
+
+  test 'should not show display name of same project member without permission' do
+    Role.find(1).remove_permission! :view_members_displayname
+    User.current = User.find(2)
+    with_settings(
+      user_format: 'displayname',
+      plugin_redmine_privacy: { 'displayname_fallback' => 'lastname' }
+    ) do
+      user = User.find @user.id
+      assert_equal 'Developer', user.name
+    end
+  end
+
+  test 'should not show display name of another project member with permission' do
+    Role.find(2).add_permission! :view_members_displayname
+    User.current = User.find(8)
+    with_settings(
+      user_format: 'displayname',
+      plugin_redmine_privacy: { 'displayname_fallback' => 'lastname' }
+    ) do
+      user = User.find @user.id
+      assert_equal 'Developer', user.name
+    end
+  end
+
 end
 
