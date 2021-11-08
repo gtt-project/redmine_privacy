8214b7eed5a569b6731cb261a78ea564e21fba83
Ko Nagase <nagase@georepublic.co.jp>
2021-11-09 06:24:01 +0900

    Updated projects controller patch for Redmine4.2

--- lib/redmine_privacy/patches/projects_controller_patch.rb
+++ lib/redmine_privacy/patches/projects_controller_patch.rb
@@ -10,7 +10,11 @@ module RedminePrivacy
       def respond_to(*_, &block)
         if params[:action] == 'show'
           # having no users effectively hides the project members box
-          @users_by_role = []
+          if Redmine::VERSION.to_a[0] >= 4 && Redmine::VERSION.to_a[1] >= 2
+            @principals_by_role = []
+          else
+            @users_by_role = []
+          end
         end
         super(*_, &block)
       end
